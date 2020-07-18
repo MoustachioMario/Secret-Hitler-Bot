92c9362e7f5bd807627d7c2add1c8800e9441a40
MoustachioMario <57885890+MoustachioMario@users.noreply.github.com>
2020-07-18 12:06:13 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -765,7 +765,8 @@ function mainLoop(){
                                 Game[i].votes[vote] = voteArray[Math.floor(Math.random() * 2)];
                             }
                         }
-                        everyoneVoted(i);
+                        updateDB(Game[i].id, JSON.stringify({"Votes":Game[i].votes}))
+                        //everyoneVoted(i);
                         break;
                     default:
                         client.users.cache.get("642172417417936925").send("Default");
